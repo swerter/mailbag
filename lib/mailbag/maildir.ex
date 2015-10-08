cdec549b08a77dff73b166cccc791e4ac32d6b8e
Michael Bruderer <ojos@gmx.ch>
2015-10-08 16:02:52 +0200

    Added parts of Mailibex to decode subject.

--- lib/mailbag/maildir.ex
+++ lib/mailbag/maildir.ex
@@ -4,20 +4,44 @@ defmodule Mailbag.Maildir do
   """
   use Timex
 
+
   @doc """
-  Extract a list of all emails in a maildir folder
+  On a mailserver, the email is often stored as followes for
+  the user aaa@bbb.com
+  /bbb.com/aaa/{INBOX,Drafts,...}
+  mailbox_path converts the email to such a path.
   ## Example
-      iex> emails = #{__MODULE__}.all("test/data")
+      iex> Mailbag.Maildir.mailbox_path("/x/", "aaa@bbb.com")
+      "/x/bbb.com/aaa/INBOX"
+      iex> Mailbag.Maildir.mailbox_path("/x/", "aaa@bbb.com", "Draft")
+      "/x/bbb.com/aaa/Draft"
+
   """
-  def all(email, folder \\ "INBOX") do
-    mailbox_path = mailbox_path(email, folder)
-    unless is_maildir?(mailbox_path), do: raise "Not a maildir"
-    IO.puts mailbox_path
+  def mailbox_path(base_path, email, folder \\ "INBOX") do
+    [user_name, domain] = String.split(email, "@")
 
-    {:ok, new_emails} = File.ls(Path.join(mailbox_path, "new"))
-    {:ok, current_emails} = File.ls(Path.join(mailbox_path, "cur"))
+    base_path
+      |> Path.join(domain)
+      |> Path.join(user_name)
+      |> Path.join(folder)
+      |> Path.expand
+  end
 
-    Enum.map(new_emails, fn(x) -> parse_email_header(Path.join(mailbox_path, "new"), x, email) end)
+  @doc """
+  Extract a list of all emails in a maildir folder, including 'cur', and 'new'.
+  ## Example
+      iex> emails = Mailbag.Maildir.all("test/data/INBOX") |> Enum.count
+      4
+
+  """
+  def all(maildir_path) do
+    unless is_maildir?(maildir_path), do: raise "Not a maildir"
+
+    {:ok, new_emails} = File.ls(Path.join(maildir_path, "new"))
+    {:ok, current_emails} = File.ls(Path.join(maildir_path, "cur"))
+
+    Enum.map(new_emails, fn(x) -> parse_email_header(Path.join(maildir_path, "new"), x) end) ++
+      Enum.map(current_emails, fn(x) -> parse_email_header(Path.join(maildir_path, "cur"), x) end)
   end
 
 
@@ -42,7 +66,7 @@ defmodule Mailbag.Maildir do
   Uses streams to avoid having to read the whole email just to
   extract the header values.
   """
-  def parse_email_header(folder_path, email_id, email) do
+  def parse_email_header(folder_path, email_id) do
     email_file_path = Path.join(folder_path, email_id)
 
     content_stream = File.stream!(email_file_path)
@@ -53,15 +77,29 @@ defmodule Mailbag.Maildir do
       from_email = Regex.run(~r/.*<(.*)>/, from) |> List.last
     end
     subject = extract_from_header(content_stream, "Subject")
-    date = DateFormat.format!(extract_from_header(content_stream, "Date"), "{ISO}")
+    |> Mailbag.MimeMail.Words.word_decode
+    date = extract_from_header(content_stream, "Date") |> date_cleansing
+    date = case Timex.DateFormat.parse(date, "{RFC1123}") do
+      {:ok, date} -> date
+      {_, date} -> raise "Could not parse date: #{extract_from_header(content_stream, "Date")}"
+    end
+
     content_type = extract_from_header(content_stream, "Content-Type")
-    %{email_address: email, id: email_id, from: from, from_email: from_email, subject: subject, date: date, content_type: content_type}
+    %{id: email_id, from: from, from_email: from_email, subject: subject, date: date, content_type: content_type}
+  end
+
+
+  defp date_cleansing(date) do
+    # remove '(CEST)' in "Mon, 5 Oct 2015 13:36:10 +0200 (CEST)"
+    case Regex.run( ~r/(.*)\s(\(.*\))/, date) do
+      [_, cleaned_date, _] -> cleaned_date
+      nil -> date
+    end
   end
 
   # Extracts the field "type" from the email header
   # Example extract_from_header(stream, "From") would return the email sender, eg. "Hans Huber <h.h@blue.com>"
   def extract_from_header(stream, type) do
-    IO.puts "hello extract_from_header: #{type}"
     res = Stream.take_while(stream, &(&1 != "\n")) # get header
     |> Stream.filter(&(Regex.match?(~r/^\S*?#{type}:.*\n/, &1))) |> Enum.at(0) # extract from header the field
     if res == 0 do # if not found, return empty string
@@ -82,14 +120,4 @@ defmodule Mailbag.Maildir do
     File.dir?(Path.join(path, 'tmp'))
   end
 
-  def mailbox_path(email, folder \\ "INBOX") do
-    base_path    = Application.get_env(:migadu, Migadu.Maildir)[:base_path]
-    [user_name, domain] = String.split(email, "@")
-
-    base_path
-      |> Path.join(domain)
-      |> Path.join(user_name)
-      |> Path.join(folder)
-      |> Path.expand
-  end
 end
--- test/maildir_test.exs
+++ test/maildir_test.exs
@@ -3,9 +3,18 @@ defmodule MaildirTests do
   doctest Mailbag.Maildir
 
 
-  test "list all emails in a maildir" do
-    maildir_path = "test/data/"
+  test "list all emails in a maildir and check subjects" do
+    maildir_path = "test/data/INBOX"
     emails = Mailbag.Maildir.all(maildir_path)
+    subjects = Enum.map(emails, &(&1.subject))
+    assert Enum.any?(subjects, &(String.match?(&1,~r/15% Rabatt auf Flyer & Falzflyer/)))
+  end
+
+  test "list all emails in a maildir and check subjects (with encoding)" do
+    maildir_path = "test/data/INBOX"
+    emails = Mailbag.Maildir.all(maildir_path)
+    subjects = Enum.map(emails, &(&1.subject))
+    assert Enum.any?(subjects, &(String.match?(&1,~r/¿TE IMAGINAS ENVIOS EN 24 HORAS?/)))
   end
 
 end
